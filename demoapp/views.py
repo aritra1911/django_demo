c806ee08a92425f5076f3d5bf29bcf00dcf79da7
Aritra Sarkar <aritra.sarkar@bajajfinserv.in>
2023-03-01 08:57:23 +0000

    Hack in methods for CustomerBankAccountViewSet
     - We overrode the list(), put(), and patch() methods of
       CustomerBankAccountViewSet as well.
    
     - This makes the DefaultRouter be able to finally work with the fully
       custom (along with the overidden methods) CustomerBankAccountViewSet.
    
     - CustomerBankAccountViewSet can hence only operate on one record that
       is the active bank account.
    
     - Respond with 404 whenever we see an id being issued in the URL
       endpoint since we do not support retrieveing or updating arbitrary
       bank accounts anymore.
    
     - Since during an update operation, we need not validate the choice of
       bank if it doesn't change, we exclude the account being updated while
       validating a bank account.
    
     - Quieten MyPy wrongly suspecting a type conflict in a few places using
       # type: ignore.

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, mixins, authentication, parsers, renderers, permissions
+from rest_framework import (
+    viewsets, mixins, authentication, parsers, renderers, permissions, status
+)
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -48,8 +50,8 @@ class CustomerViewSet(mixins.CreateModelMixin,
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        if not self.request.user.is_superuser:
-            queryset = queryset.filter(id=self.request.user.id)
+        if not self.request.user.is_superuser:  # type: ignore
+            queryset = queryset.filter(id=self.request.user.id) # type: ignore
         return queryset
 
 
@@ -82,13 +84,38 @@ class CustomerBankAccountViewSet(viewsets.ModelViewSet):
 
         serializer.save(customer=customer)
 
+    def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        return self.retrieve(request, *args, **kwargs)
+
     def retrieve(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        if kwargs.get('pk'):
+            return Response({ 'detail': (
+                'You are restricted to fetching only the active account. '
+                'You cannot fetch an arbitrary account here.'
+            )}, status=status.HTTP_404_NOT_FOUND)
+
         active_account: CustomerBankAccount = self.get_object()
         return Response(self.get_serializer(active_account).data)
 
     def update(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        if kwargs.get('pk'):
+            return Response({ 'detail': (
+                'You are restricted to updating only the active account. '
+                'You cannot update any arbitrary account.'
+            )}, status=status.HTTP_404_NOT_FOUND)
+
         active_account = self.get_object()
-        serializer = self.get_serializer(active_account, data=request.data, partial=True)
+        serializer = self.get_serializer(
+            active_account,
+            data=request.data,
+            partial=True
+        )
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
