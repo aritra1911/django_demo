78f501f317c9d4b1b10115b92b0d07df25b12ea1
Aritra Sarkar <aritra.sarkar@bajajfinserv.in>
2023-03-08 06:43:22 +0000

    Test Customer API
     - Also add type annotations.

--- demoapp/tests/test_customer.py
+++ demoapp/tests/test_customer.py
@@ -1,49 +1,60 @@
 import pytest
 from django.core.exceptions import ValidationError
+from django.urls import reverse
+from rest_framework.test import APIClient
 from demoapp.models import Customer
 from typing import Dict
 
-TEST_DATA_1 = {
-    'email': 'testuser@example.com',
-    'password': 'password',
-    'first_name': 'John',
-    'last_name': 'Doe',
-    'middle_name': 'M',
-    'pan_number': '1A2B3C4D5E',
-    'full_name': 'John M Doe',
+TEST_DATA_1: Dict[str, str] = {
+    "email": "testuser@example.com",
+    "password": "password",
+    "first_name": "John",
+    "last_name": "Doe",
+    "middle_name": "M",
+    "pan_number": "1A2B3C4D5E",
+    "full_name": "John M Doe",
 }
 
 TEST_DATA_2 = {
-    'email': 'testuser2@example.com',
-    'password': 'password',
-    'first_name': 'John',
-    'last_name': 'Doe',
-    'pan_number': '1A2B3C4D5F',
-    'full_name': 'John Doe',
+    "email": "testuser2@example.com",
+    "password": "password",
+    "first_name": "John",
+    "last_name": "Doe",
+    "pan_number": "1A2B3C4D5F",
+    "full_name": "John Doe",
 }
 
 def create_test_customer(test_data: Dict[str, str]) -> Customer:
     return Customer.objects.create_user(
-        email=test_data.get('email'),
-        password=test_data.get('password'),
-        first_name=test_data.get('first_name'),
-        last_name=test_data.get('last_name'),
-        middle_name=test_data.get('middle_name'),
-        pan_number=test_data.get('pan_number')
+        email=test_data.get("email"),
+        password=test_data.get("password"),
+        first_name=test_data.get("first_name"),
+        last_name=test_data.get("last_name"),
+        middle_name=test_data.get("middle_name"),
+        pan_number=test_data.get("pan_number")
     )
 
-@pytest.mark.django_db
-def test_create_customer() -> None:
-    test_data: Dict[str, str] = TEST_DATA_1
-    customer: Customer = create_test_customer(test_data)
 
+@pytest.fixture
+def client() -> APIClient:
+    return APIClient()
+
+
+@pytest.fixture
+def customer() -> Customer:
+    return create_test_customer(TEST_DATA_1)
+
+
+@pytest.mark.django_db
+def test_create_customer(customer: Customer) -> None:
     # Check that the customer was created successfully
-    assert customer.first_name == test_data['first_name']
-    assert customer.last_name == test_data['last_name']
-    assert customer.email == test_data['email']
+    assert customer.first_name == TEST_DATA_1['first_name']
+    assert customer.last_name == TEST_DATA_1['last_name']
+    assert customer.email == TEST_DATA_1['email']
 
     # Cleanup
     customer.delete()
+
 
 @pytest.mark.django_db
 def test_create_customer_null_required_fields() -> None:
@@ -62,6 +73,7 @@ def test_create_customer_null_required_fields() -> None:
             test_data.pop(field)
             create_test_customer(test_data)
 
+
 @pytest.mark.django_db
 def test_create_customer_blank_required_fields() -> None:
     """
@@ -79,6 +91,7 @@ def test_create_customer_blank_required_fields() -> None:
             test_data[field] = ""
             create_test_customer(test_data)
 
+
 @pytest.mark.django_db
 def test_create_customer_validate_fields() -> None:
     """
@@ -93,6 +106,7 @@ def test_create_customer_validate_fields() -> None:
             test_data[field] = "testuser"
             create_test_customer(test_data)
 
+
 @pytest.mark.django_db
 def test_customer_str() -> None:
     for test_data in (TEST_DATA_1, TEST_DATA_2):
@@ -101,3 +115,58 @@ def test_customer_str() -> None:
 
         # Check that the __str__ method returns the expected string
         assert str(customer) == test_data['full_name']
+
+
+@pytest.mark.django_db
+def test_api_create_customer(client: APIClient) -> None:
+    # Ensure that we can create a new customer without any kind of
+    # authentication.
+    response = client.post(reverse("customer-list"), data=TEST_DATA_1)
+    assert response.status_code == 201
+    assert response.data["email"] == "testuser@example.com"
+    assert response.data["first_name"] == "John"
+    assert response.data["last_name"] == "Doe"
+    assert response.data["middle_name"] == "M"
+    assert response.data["pan_number"] == "1A2B3C4D5E"
+
+
+@pytest.mark.django_db
+def test_api_create_duplicate_customer(
+    client: APIClient,
+    customer: Customer
+) -> None:
+    # Ensure that we can NOT create a duplicate customer
+    response = client.post(reverse("customer-list"), data=TEST_DATA_1)
+    assert response.status_code == 400
+    assert "email" in response.data
+    assert "pan_number" in response.data
+
+
+@pytest.mark.django_db
+def test_api_retrieve_customer(client: APIClient, customer: Customer) -> None:
+    client.force_authenticate(user=customer)
+    response = client.get(reverse("customer-list"))
+    assert response.status_code == 200
+    assert len(response.data) == 1
+    assert response.data[0]["email"] == customer.email
+    assert response.data[0]["first_name"] == customer.first_name
+    assert response.data[0]["last_name"] == customer.last_name
+    assert response.data[0]["pan_number"] == customer.pan_number
+
+
+@pytest.mark.django_db
+def test_api_update_customer(client: APIClient, customer: Customer) -> None:
+    client.force_authenticate(user=customer)
+    response = client.patch(
+        reverse("customer-detail", args=(customer.id,)),        # type: ignore
+        data={
+            "first_name": "Filthy",
+            "last_name": "Frank",
+            "pan_number": "GQZPA7298D",
+        }
+    )
+    assert response.status_code == 200
+    assert response.data["email"] == customer.email
+    assert response.data["first_name"] == "Filthy"
+    assert response.data["last_name"] == "Frank"
+    assert response.data["pan_number"] == "GQZPA7298D"
--- demoapp/tests/test_customer_bank_account.py
+++ demoapp/tests/test_customer_bank_account.py
@@ -1,17 +1,17 @@
 import pytest
-from django.contrib.auth.models import User
+from django.urls import reverse
 from rest_framework.test import APIClient
 from demoapp.models import Customer, Bank, CustomerBankAccount
-from pprint import pprint
+from typing import Dict
 
 
 @pytest.fixture
-def client():
+def client() -> APIClient:
     return APIClient()
 
 
 @pytest.fixture
-def customer():
+def customer() -> Customer:
     return Customer.objects.create(
         email='testuser@example.com',
         password='testpass',
@@ -21,15 +21,14 @@ def customer():
 
 
 @pytest.fixture
-def bank():
+def bank() -> Bank:
     return Bank.objects.create(
-        name='Test Bank',
-        logo='test_logo.png'
+        name='Test Bank'
     )
 
 
 @pytest.fixture
-def account(customer, bank):
+def account(customer: Customer, bank: Bank) -> CustomerBankAccount:
     return CustomerBankAccount.objects.create(
         customer=customer,
         bank=bank,
@@ -40,63 +39,81 @@ def account(customer, bank):
 
 
 @pytest.mark.django_db
-def test_create_account(client, customer, bank):
+def test_create_account(
+    client: APIClient,
+    customer: Customer,
+    bank: Bank
+) -> None:
     # Ensure we can create a new account
     client.force_authenticate(user=customer)
-    data = {
-        "bank": str(bank.id),
+    data: Dict[str, str] = {
+        "bank": str(bank.id),                           # type: ignore
         "account_number": "9876543210",
         "ifsc_code": "WXYZ7654321",
         "branch_name": "Test Branch",
         "name_as_per_bank_record": "test",
     }
-    response = client.post('/api/bank/', data)
+    response = client.post(reverse("active-bank-list"), data)
     assert response.status_code == 201
-    assert response.data['bank'] == bank.id
-    assert response.data['account_number'] == '9876543210'
-    assert response.data['ifsc_code'] == 'WXYZ7654321'
-    assert response.data['is_active'] is True
+    assert response.data["bank"] == bank.id             # type: ignore
+    assert response.data["account_number"] == "9876543210"
+    assert response.data["ifsc_code"] == "WXYZ7654321"
+    assert response.data["is_active"] is True
 
 
 @pytest.mark.django_db
-def test_create_duplicate_active_account(client, customer, bank, account):
+def test_create_duplicate_active_account(
+    client: APIClient,
+    customer: Customer,
+    bank: Bank,
+    account: CustomerBankAccount
+) -> None:
     # Ensure we can't create a duplicate account
     client.force_authenticate(user=customer)
-    data = {
-        "bank": str(bank.id),
+    data: Dict[str, str] = {
+        "bank": str(bank.id),                           # type: ignore
         "account_number": account.account_number,
         "ifsc_code": account.ifsc_code,
         "branch_name": "Test Branch",
         "name_as_per_bank_record": "test",
     }
-    response = client.post('/api/bank/', data)
+    response = client.post(reverse("active-bank-list"), data)
     assert response.status_code == 400
-    assert 'non_field_errors' in response.data
+    assert "non_field_errors" in response.data
 
 
 @pytest.mark.django_db
-def test_retrieve_active_account(client, customer, account):
+def test_retrieve_active_account(
+    client: APIClient,
+    customer: Customer,
+    account: CustomerBankAccount
+) -> None:
     # Ensure we can retrieve the active account
     client.force_authenticate(user=customer)
-    response = client.get('/api/bank/')
+    response = client.get(reverse("active-bank-list"))
     assert response.status_code == 200
-    assert response.data['bank'] == account.bank.id
-    assert response.data['account_number'] == account.account_number
-    assert response.data['ifsc_code'] == account.ifsc_code
-    assert response.data['is_active'] == account.is_active
+    assert response.data["bank"] == account.bank.id
+    assert response.data["account_number"] == account.account_number
+    assert response.data["ifsc_code"] == account.ifsc_code
+    assert response.data["is_active"] is True
 
 
 @pytest.mark.django_db
-def test_update_account(client, customer, account):
+def test_update_account(
+    client: APIClient,
+    customer: Customer,
+    account: CustomerBankAccount
+) -> None:
     # Ensure we can update the active account
     client.force_authenticate(user=customer)
-    data = {
-        'bank': account.bank.id,
+    data: Dict[str, str] = {
+        "bank": str(account.bank.id),
+        "name_as_per_bank_record": "test2",
     }
-    response = client.patch('/api/bank/', data)
-    print(response.data)
+    response = client.patch(reverse("active-bank-list"), data)
     assert response.status_code == 200
-    assert response.data['bank'] == account.bank.id
-    assert response.data['account_number'] == account.account_number
-    assert response.data['ifsc_code'] == account.ifsc_code
-    assert response.data['is_active'] == account.is_active
+    assert response.data["bank"] == account.bank.id
+    assert response.data["account_number"] == account.account_number
+    assert response.data["ifsc_code"] == account.ifsc_code
+    assert response.data["is_active"] == account.is_active
+    assert response.data["name_as_per_bank_record"] == "test2"
